86867139fdb82899f283a875e922b72784c6d3a6
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-09-24 22:51:43 +0100

    add same packages to the app

---
+++ modules/Blog/Blog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Facturis\Blog;
+
+class Blog {
+
+    public function tryBlog()
+    {
+        return "Hello Abdelghafour from Blog";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Facturis\Blog\Blog;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/try', function () {
+    $blog = new Blog();
+
+    return $blog->tryBlog();
 });
