0187808105b47a17bdeeaa48f2f1db32428c5042
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2022-10-17 12:49:24 +0100

    add laravel Fortify to the app

--- modules/Estimates/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('estimates::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('estimates.name') !!}
-    </p>
-@endsection
