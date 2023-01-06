a1e82b7fd3856b16b6cfb9c881b0027ba114685e
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-01-06 17:22:00 +0100

    remove laravel/foritfy

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-        App\Providers\FortifyServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -194,7 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        //App\Providers\TenancyServiceProvider::class,
+        App\Providers\TenancyServiceProvider::class,
 
         HTMLMin\HTMLMin\HTMLMinServiceProvider::class,
 
