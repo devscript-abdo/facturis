42e70289eff41652b1cd3cb0a8808edfbec11d00
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-01-09 18:34:43 +0100

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\RedirectorMiddleware::class,
 
         \HTMLMin\HTMLMin\Http\Middleware\MinifyMiddleware::class,
 
---
+++ app/Http/Middleware/RedirectorMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+
+class RedirectorMiddleware
+{
+    /**
+     * @param  Request  $request
+     * @param  Closure  $next
+     * @return Application|RedirectResponse|Redirector|mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->is('app', 'app/')) {
+            return redirect()->route('facturis:home');
+        }
+
+        if ($request->is('/', '/home')) {
+            return redirect()->route('facturis:home');
+        }
+
+        return $next($request);
+    }
+}
