05bc57c8cb0976aa8b40e1590fa2e98a2d3cf1d1
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-01-25 18:10:05 +0100

    update same stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>facturis app</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-       @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased">
-        <div>
+    <title>facturis app</title>
 
-        
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}"">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Log in</a>
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-            
-            <p align="center">
-                <a href="https://facturis.ma" target="_blank">
-                    <img src="{{asset('facturis/screen.png')}}" >
-                </a>
-            </p>
-        </div>
-    </body>
+<body class="antialiased">
+    <div>
+
+
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/home') }}"">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+        <p align="center">
+            <a href="https://facturis.ma" target="_blank">
+                <img src="{{ asset('facturis/screen.png') }}">
+            </a>
+        </p>
+    </div>
+</body>
+
 </html>
